e54aec48b47d033acd64aec1c13d32ab33dee194
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-02-21 12:30:02 -0800

    add classes

--- AnagramTracker/Models/Anagram.cs
+++ AnagramTracker/Models/Anagram.cs
@@ -1,4 +1,37 @@
-public class Anagram
+namespace AnagramTracker.Models
 {
-  // Code for Triangle business logic will go here.
+  public class Anagram
+  {
+    private string _targetWord;
+
+    public Anagram(string targetWord)
+    {
+      _targetWord = targetWord;
+    }
+    public string GetTargetWord()
+    {
+      return _targetWord;
+    }
+    // public string SetTargetWord(string targetWord)
+    // {
+    //   _targetWord = targetWord;
+    // }
+    public bool CheckAnagram(string suggestedWords)
+    {
+      if (_targetWord.Sort() == suggestedWords.Sort())
+      {
+        return true;
+      }
+      else
+        return false;
+
+    }
+
+
+
+
+
+
+
+  }
 }
--- AnagramTracker/Program.cs
+++ AnagramTracker/Program.cs
@@ -1,10 +1,49 @@
 using System;
+using AnagramTracker.Models;
 
-class Program
+namespace AnagramTracker
 {
-  static void Main()
+  class Program
   {
-    Anagram testTriangle = new Anagram();
-    Console.WriteLine(testTriangle.GetType());
+    static void Main()
+    {
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to the Anagram app!");
+      Console.WriteLine("Please enter in a word:");
+      string targetWord = Console.ReadLine();
+      Anagram ana = new Anagram(targetWord);
+      suggestedPromp();
+
+
+      static void suggestedPromp()
+      {
+        Console.WriteLine("Please enter in a suggested word:");
+        string suggestedWord = Console.ReadLine();
+        bool result = ana.CheckAnagram(suggestedWord);
+
+        if (result == true)
+        {
+          Console.WriteLine("Your suggested word is an anagram!");
+
+        }
+        else
+        {
+          Console.WriteLine("Sorry your suggested word is NOT anagram!");
+        }
+        Console.WriteLine("Do you want to add another suggested word? Enter 'yes' to proceed, or 'no' to end the greatest app ever created");
+        string userInput = Console.ReadLine();
+        if (userInput == "yes")
+        {
+          suggestedPromp();
+
+        }
+        else
+        {
+          Console.WriteLine("Your loss buddy! Peace out");
+
+        }
+      }
+
+    }
   }
 }
