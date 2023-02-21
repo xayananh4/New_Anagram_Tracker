b752421319a0266bb80c2c13bd477d38a0317f55
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-02-21 10:56:07 -0800

    initial commit

---
+++ AnagramTracker/Models/Anagram.cs
@@ -0,0 +1,4 @@
+public class Anagram
+{
+  // Code for Triangle business logic will go here.
+}
---
+++ AnagramTracker/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+  static void Main()
+  {
+    Anagram testTriangle = new Anagram();
+    Console.WriteLine(testTriangle.GetType());
+  }
+}
